d89f71aa24ad27d8dc3baa69d7fa10aee94560a4
Anibal Ventura <anibalventura20@gmail.com>
2022-01-03 11:09:16 -0400

    Add UI for main and launch screen and add app icon.

--- CoinCheck/Controllers/CoinController.swift
+++ CoinCheck/Controllers/CoinController.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+struct CoinController {
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+}
