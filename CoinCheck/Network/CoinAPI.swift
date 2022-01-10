805cf8facde6550ae2967651d38bd94757249510
Anibal Ventura <48188840+anibalventura@users.noreply.github.com>
2022-01-10 18:40:44 -0400

    Add CoinAPI.swift

---
+++ CoinCheck/Network/CoinAPI.swift
@@ -0,0 +1,13 @@
+//
+//  CoinAPI.swift
+//  Coin Check
+//
+//  Created by Anibal Ventura on 3/1/22.
+//
+
+import Foundation
+
+struct CoinAPI {
+    static let key: String = "YOUR_API_KEY_HERE"
+    static let baseURL: String = "https://rest.coinapi.io/v1/exchangerate/BTC"
+}
