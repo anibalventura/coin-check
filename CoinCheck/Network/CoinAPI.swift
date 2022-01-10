cba13620d23065b27c1825ff2b4e02b1ae56c4c1
Anibal Ventura <48188840+anibalventura@users.noreply.github.com>
2022-01-10 18:43:06 -0400

    Update CoinAPI.swift

--- CoinCheck/Network/CoinAPI.swift
+++ CoinCheck/Network/CoinAPI.swift
@@ -9,5 +9,5 @@ import Foundation
 
 struct CoinAPI {
     static let key: String = "YOUR_API_KEY_HERE"
-    static let baseURL: String = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    static let baseURL: String = "https://rest.coinapi.io/v1/exchangerate"
 }
